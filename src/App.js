96b0c53297516ec6d6976d984ed4dcf64b0994fc
Abdullah Ali <abdullah.ali@cloudcardinc.com>
2023-03-01 17:36:41 +0500

    done except responsiveness & not pixel perfect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import helloImg from './hello.png';
+import Background from './Frame.svg';
+import sideImg from './Rectangle.svg';
+import { Input } from 'antd';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const { TextArea } = Input;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main">
+      <div class='left-col'>
+
+        <div class='hello-img' >
+          <img src={helloImg}></img>
+          <p class='hello-text'>Hello</p>
+        </div>
+        <div>
+          <p class = 'bold'>abc@email.com</p>
+          <p class = 'bold'>232-324-2343</p>
+          <p>dadsad sdfds 234324</p>
+        </div>
+        <div class='background-img' style={{backgroundImage: `url(${Background})` }}>
+          <img class='side-img' src={sideImg}></img>
+        </div>
+
+      </div>
+
+      <div class='right-col'>
+        <form>
+          <p class = 'bold'>Full Name</p>
+          <Input class='input' type="text" name="name" />
+
+          <p class = 'bold'>Email Address</p>
+          <Input class='input' type="text" name="email" />
+
+
+          <p class = 'bold'>Phone Number</p>
+          <Input class='input' type="text" name="phone" />
+
+
+          <p class = 'bold'>Message</p>
+          <TextArea class='text-area' type="text" name="name" rows={4} />
+
+          <input class='send-btn' type="submit" value="Send Message" />
+        </form>
+
+        <div class = 'end-text'>
+          <p>This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply</p>
+        </div>
+      </div>
     </div>
   );
 }
